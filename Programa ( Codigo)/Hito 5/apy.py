6f7012805dc7bd13f7a95b0332ee7033848a93bd
MatiasPerez16 <114115044+MatiasPerez16@users.noreply.github.com>
2023-11-20 01:50:47 -0300

    Funciona la busqueda de talleristas
    Boton que permite buscar los talleristas y insumos recomendados .Tambien funciona busqueda de google de talleristas pero no el de insumos

--- Programa ( Codigo)/Hito 5/apy.py
+++ Programa ( Codigo)/Hito 5/apy.py
@@ -6,7 +6,7 @@
 
 from flask import Flask, jsonify, request
 from openai_operations import configure_api_key, obtener_tipo_tallerista, obtener_insumos
-from google_search_operations import configure_google_credentials, buscar_tallerista, buscar_insumos
+from google_search_operations import buscar_tallerista, buscar_insumos
 
 app = Flask(__name__)
 
@@ -17,34 +17,49 @@ def configure_openai_key():
     configure_api_key(api_key)
     return jsonify({'message': 'OpenAI API Key configured successfully'})
 
+# Almacenar la información de tipo de tallerista e insumos
+tipo_tallerista_cache = {}
+insumos_cache = {}
+
 # Endpoint para obtener tipo de tallerista
 @app.route('/tipo_tallerista', methods=['POST'])
 def get_tipo_tallerista():
     descripcion = request.json.get('descripcion')
-    tipo_tallerista = obtener_tipo_tallerista(descripcion)
-    return jsonify({'tipo_tallerista': tipo_tallerista})
+    tipo_tallerista_cache[descripcion] = obtener_tipo_tallerista(descripcion)
+    return jsonify({'tipo_tallerista': tipo_tallerista_cache[descripcion]})
 
-# Endpoint para obtener insumos
+# Endpoint para obtener recomendación de insumos
 @app.route('/insumos', methods=['POST'])
 def get_insumos():
     descripcion = request.json.get('descripcion')
-    insumos = obtener_insumos(descripcion)
-    return jsonify({'insumos': insumos})
+    insumos_cache[descripcion] = obtener_insumos(descripcion)
+    return jsonify({'insumos': insumos_cache[descripcion]})
 
-# Endpoint para buscar tallerista en Google
+# Endpoint para buscar tallerista en Google.
+
 @app.route('/buscar_tallerista', methods=['POST'])
 def search_tallerista():
-    query = request.json.get('query')
-    resultados = buscar_tallerista(query)
-    return jsonify({'resultados': resultados})
+    descripcion = request.json.get('descripcion')
+    if descripcion not in tipo_tallerista_cache:
+        return jsonify({'error': 'Primero debe obtener el tipo de tallerista para la descripción dada'})
 
-# Endpoint para buscar insumos en Google
+    resultados = buscar_tallerista(tipo_tallerista_cache[descripcion])
+    table = [{'title': resultado['title'], 'link': resultado['link']} for resultado in resultados]
+    return jsonify({'tabla_tallerista': table})
+
+# Endpoint para buscar insumos en Google.
 @app.route('/buscar_insumos', methods=['POST'])
 def search_insumos():
-    query = request.json.get('query')
-    resultados = buscar_insumos(query)
-    return jsonify({'resultados': resultados})
+    descripcion = request.json.get('descripcion')
+    if descripcion not in insumos_cache:
+        return jsonify({'error': 'Primero debe obtener los insumos para la descripción dada'})
+
+    resultados = buscar_insumos(insumos_cache[descripcion])
+    table = [{'title': resultado['title'], 'link': resultado['link']} for resultado in resultados]
+    return jsonify({'tabla_insumos': table})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
--- Programa ( Codigo)/Hito 5/streamlit_interface.py
+++ Programa ( Codigo)/Hito 5/streamlit_interface.py
@@ -6,43 +6,104 @@
 
 import streamlit as st
 import requests
+import pandas as pd
 
 # URL base de la API Flask
 BASE_URL = 'http://localhost:5000'  # Cambia esto por la URL donde se ejecute la API Flask
+
+# Definir estilos CSS para la interfaz
+estilos = """
+    <style>
+    body {
+        color: #ffffff; /* Texto blanco */
+        background-color: #6a0572; /* Fondo morado */
+    }
+    .st-bw {
+        color: #000000; /* Texto negro */
+    }
+    .stButton > button {
+        background-color: #f4b41a; /* Botón amarillo */
+    }
+    </style>
+"""
+
+# Aplicar estilos
+st.markdown(estilos, unsafe_allow_html=True)
 
 # Configurar título y descripción de la aplicación Streamlit
 st.title('Búsqueda de Talleristas e Insumos')
 st.write('Esta aplicación interactúa con la API para realizar búsquedas y mostrar los resultados.')
 
-# Sección para obtener la API Key de OpenAI
+# Sección para configurar la API Key de OpenAI
 st.header('Configurar API Key de OpenAI')
 api_key = st.text_input('Introduce tu API Key de OpenAI:')
+
+# Botón para configurar la API Key
 if st.button('Configurar API Key'):
-    response = requests.post(f'{BASE_URL}/configure_openai_key', json={'api_key': api_key})
-    if response.status_code == 200:
+    response_api_key = requests.post(f'{BASE_URL}/configure_openai_key', json={'api_key': api_key})
+    if response_api_key.status_code == 200:
         st.success('API Key de OpenAI configurada correctamente.')
     else:
         st.error('Error al configurar la API Key de OpenAI.')
 
-# Sección para obtener el tipo de tallerista
-st.header('Obtener Tipo de Tallerista')
-descripcion_tallerista = st.text_input('Ingresa la descripción para obtener el tipo de tallerista:')
-if st.button('Obtener Tipo de Tallerista'):
-    response = requests.post(f'{BASE_URL}/tipo_tallerista', json={'descripcion': descripcion_tallerista})
+
+# Sección para obtener la descripción y realizar acciones
+st.header('Descripción para búsqueda')
+descripcion = st.text_input('Ingresa la descripción:')
+
+# Variable para almacenar los insumos recomendados
+insumos = []
+
+# Botón para realizar el análisis de la descripción y obtener recomendaciones
+if st.button('Obtener Recomendación'):
+    response = requests.post(f'{BASE_URL}/tipo_tallerista', json={'descripcion': descripcion})
     if response.status_code == 200:
         tipo_tallerista = response.json()['tipo_tallerista']
         st.success(f'Tipo de tallerista recomendado: {tipo_tallerista}')
-    else:
-        st.error('Error al obtener el tipo de tallerista.')
 
-# Sección para obtener insumos recomendados
-st.header('Obtener Insumos Recomendados')
-descripcion_insumos = st.text_input('Ingresa la descripción para obtener insumos recomendados:')
-if st.button('Obtener Insumos Recomendados'):
-    response = requests.post(f'{BASE_URL}/insumos', json={'descripcion': descripcion_insumos})
-    if response.status_code == 200:
-        insumos_recomendados = response.json()['insumos']
-        st.success(f'Insumos recomendados: {insumos_recomendados}')
+        insumos_response = requests.post(f'{BASE_URL}/insumos', json={'descripcion': descripcion})
+        if insumos_response.status_code == 200:
+            insumos = insumos_response.json()['insumos']
+            st.success(f'Insumos recomendados: {insumos}')
     else:
-        st.error('Error al obtener insumos recomendados.')
+        st.error('Error al realizar el análisis.')
+
+# Botón para buscar talleristas en Google
+if st.button('Buscar Talleristas con Google'):
+    response_google_tallerista = requests.post(f'{BASE_URL}/buscar_tallerista', json={'descripcion': descripcion})
+    if response_google_tallerista.status_code == 200:
+        resultados_google_tallerista = response_google_tallerista.json().get('tabla_tallerista')
+        st.subheader('Resultados de búsqueda en Google por Tipo de Tallerista')
+        for resultado in resultados_google_tallerista:
+            st.write(f"Descripción: {resultado['title']}")
+            st.write(f"Link: {resultado['link']}")
+            st.markdown("---")
+
+
+if st.button('Buscar Insumos en Google'):
+    if insumos:
+        # Obtener la lista de insumos únicos para el selector
+        insumos_lista = insumos.split(", ")
+
+        # Realizar la búsqueda de 10 resultados para cada insumo recomendado
+        for insumo in insumos_lista:
+            response_google_insumos = requests.post(f'{BASE_URL}/buscar_insumos', json={'descripcion': insumo})
+            if response_google_insumos.status_code == 200:
+                resultados_google_insumos = response_google_insumos.json().get('tabla_insumos')
+                
+                # Limitar los resultados a 10
+                resultados_insumo = resultados_google_insumos[:10]
+
+                # Mostrar los resultados para este insumo
+                st.subheader(f'Resultados para {insumo} en Chile:')
+                table_data = []
+                for resultado in resultados_insumo:
+                    table_data.append({'Descripción': resultado['title'], 'Link': resultado['link']})
+                
+                # Mostrar la tabla con los resultados para este insumo
+                st.write(pd.DataFrame(table_data))
+    else:
+        st.warning('Primero debes obtener la recomendación de insumos.')
+         
+
 
