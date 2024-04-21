d1a11f506cb8fe95480e4724209775a415449856
MatiasPerez16 <114115044+MatiasPerez16@users.noreply.github.com>
2024-04-21 13:23:53 -0400

    Update openai_operations.py

--- Programa(codigo 2024)/Hito2_2024/openai_operations.py
+++ Programa(codigo 2024)/Hito2_2024/openai_operations.py
@@ -10,9 +10,10 @@ def configure_api_key(api_key):
     openai.api_key = api_key
 
 def obtener_tipo_tallerista(descripcion):
+    # Llama al modelo gpt-3.5-turbo-instruct en lugar de text-davinci-003
     respuesta = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Descripción: {descripcion}\nTipo profesión recomendado para el taller ,tiene que estar en singular, solo nombrar la profesión sin nada más ni espacios:",
+        engine="gpt-3.5-turbo-instruct",
+        prompt=f"Descripción: {descripcion}\nTipo profesión recomendado para el taller, tiene que estar en singular, solo nombrar la profesión sin nada más ni espacios:",
         max_tokens=50,
     )
 
@@ -20,12 +21,12 @@ def obtener_tipo_tallerista(descripcion):
     return tipo_tallerista
 
 def obtener_insumos(descripcion):
+    # Llama al modelo gpt-3.5-turbo-instruct en lugar de text-davinci-003
     respuesta = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Descripción: {descripcion}\n Hasta 10 Insumos recomendados para el taller con su nombre completo, solo nombrarlos sin nada más y separados por comas, sin ninguna otra tipo de puntuación:",
+        engine="gpt-3.5-turbo-instruct",
+        prompt=f"Descripción: {descripcion}\nHasta 10 Insumos recomendados para el taller con su nombre completo, solo nombrarlos sin nada más y separados por comas, sin ninguna otra tipo de puntuación:",
         max_tokens=100,
     )
 
     insumos = respuesta.choices[0].text.strip()
     return insumos
-
